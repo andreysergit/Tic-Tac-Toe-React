3729d356b643834d4f6f8ea552e459e839e893ca
andreysergit <andreysergit@gmail.com>
2020-05-17 21:45:51 +0300

    react tic-tac

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./components/assets/index.css"
+import {Game} from './components/Game'
+
+
+ReactDOM.render(
+  <Game />,
+   document.getElementById("root")
+   );
